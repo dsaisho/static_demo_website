78dee4c6fb864f7d30dcb46ef89c6d38cd142363
dsaisho <dsaisho@gmail.com>
2020-09-03 16:56:48 -0700

    added dynamic destinations

--- site/js/destinations.js
+++ site/js/destinations.js
@@ -3,7 +3,86 @@ var apiData = {
         "destination_1":{
             "title": "Trickle Creek Ranch",
             "desc": "Book unique camping experiences on over 300,000 campsites.",
-            "img": "../assets/image-K-11.png"
+            "img": "assets/image-K-11.png",
+            "price": "$36/night",
+            "favorite": "active"
+        },
+        "destination_2":{
+            "title": "Dragonfly Tiny Cabin",
+            "desc": "Book unique camping experiences on over 300,000 campsites.",
+            "img": "assets/image-K-2.png",
+            "price": "$46/night",
+            "favorite": ""
+        },
+        "destination_3":{
+            "title": "Tiny Cabin in the mountains",
+            "desc": "Book unique camping experiences on over 300,000 campsites.",
+            "img": "assets/image-K-5.png",
+            "price": "$36/night",
+            "favorite": ""
+        },
+        "destination_4":{
+            "title": "The Stuga",
+            "desc": "Book unique camping experiences on over 300,000 campsites.",
+            "img": "assets/image-K-8.png",
+            "price": "$46/night",
+            "favorite": "active"
+        },
+        "destination_5":{
+            "title": "Luxury Tiny Beach Cabin",
+            "desc": "Book unique camping experiences on over 300,000 campsites.",
+            "img": "assets/image-K-12.png",
+            "price": "$36/night",
+            "favorite": ""
+        },
+        "destination_6":{
+            "title": "The Summit Cabin",
+            "desc": "Book unique camping experiences on over 300,000 campsites.",
+            "img": "assets/image-K-3.png",
+            "price": "$46/night",
+            "favorite": ""
+        },
+        "destination_7":{
+            "title": "Kindred Spirits Cabin",
+            "desc": "Book unique camping experiences on over 300,000 campsites.",
+            "img": "assets/image-K-6.png",
+            "price": "$36/night",
+            "favorite": "active"
+        },
+        "destination_8":{
+            "title": "The Hermitage Cabin",
+            "desc": "Book unique camping experiences on over 300,000 campsites.",
+            "img": "assets/image-K-9.png",
+            "price": "$46/night",
+            "favorite": ""
+        },
+        "destination_9":{
+            "title": "Red Lifeguard Stand",
+            "desc": "Book unique camping experiences on over 300,000 campsites.",
+            "img": "assets/image-K-1.png",
+            "price": "$36/night",
+            "favorite": ""
+        },
+        "destination_10":{
+            "title": "The Tree House",
+            "desc": "Book unique camping experiences on over 300,000 campsites.",
+            "img": "assets/image-K-4.png",
+            "price": "$46/night",
+            "favorite": ""
+        },
+        "destination_11":{
+            "title": "Barrier island elevated tent",
+            "desc": "Book unique camping experiences on over 300,000 campsites.",
+            "img": "assets/image-K-7.png",
+            "price": "$36/night",
+            "favorite": "active"
+        },
+        "destination_12":{
+            "title": "Ninovan on the Shore",
+            "desc": "Book unique camping experiences on over 300,000 campsites.",
+            "img": "assets/image-K-10.png",
+            "price": "$46/night",
+            "favorite": ""
         }
     }
 }
--- site/js/script.js
+++ site/js/script.js
@@ -3,24 +3,79 @@ window.onload = function() {
 };
 
 let emailActive = false;
-let emailInput;
-let emailLabel;
-let emailSubmit;
-let validity;
-let goUp;
+
+const els ={
+    emailInput:"email-input",
+    emailLabel:"email-label",
+    emailSubmit:"submit",
+    validity:"validity",
+    goUp:"go-up",
+    viewAll:"view-all",
+    exploreList:"explore-list"
+}
+let set = 1;
 
 function init(){
-    emailInput = document.getElementById('email-input');
-    emailLabel = document.getElementById('email-label');
-    emailSubmit = document.getElementById('submit');
-    validity = document.getElementById('validity');
-    goUp = document.getElementById('go-up');
+    cacheEls();
 
-    goUp.addEventListener('click', goUpClicked);
-    emailInput.addEventListener("click", emailInputClicked);
-    emailSubmit.addEventListener("click", emailSubmitClicked);
-    emailInput.addEventListener('blur', emailBlurHandler);
-    emailInput.addEventListener("keypress", emailKeyPressed);
+    els.goUp.addEventListener('click', goUpClicked);
+    els.emailInput.addEventListener("click", emailInputClicked);
+    els.emailSubmit.addEventListener("click", emailSubmitClicked);
+    els.emailInput.addEventListener('blur', emailBlurHandler);
+    els.emailInput.addEventListener("keypress", emailKeyPressed);
+    els.viewAll.addEventListener("click", viewAllClicked);
+
+    loadDestinationData(window.apiData);
+    animateHero();
+}
+function cacheEls(){
+    Object.keys(els).map(function(val,i){
+        const el = els[val];
+        els[val] = document.getElementById(el);
+    })
+}
+function animateHero(){
+    TweenLite.from('.container', .5, {opacity:0});
+    TweenLite.from('.title', 1, {opacity:0, x:-50, delay:.8});
+    TweenLite.from('.desc', 1, {opacity:0, x:50, delay:.8});
+    TweenLite.from('.discover', .5, {opacity:0, delay:1.8});
+
+    const heroLinks = document.querySelectorAll('.hero-link');
+    let delayValue = 2.3;
+    for(let i = 0; i < heroLinks.length; i++){
+        TweenLite.to(heroLinks[i],.2,{y:-20, delay:delayValue});
+        TweenLite.to(heroLinks[i],.2,{y:0, delay:delayValue +.2})
+        delayValue += .1;
+    }
+}
+
+function viewAllClicked(){
+    loadDestinationData(window.apiData);
+    TweenLite.to(viewAll,1,{autoAlpha:0})
+}
+
+function loadDestinationData(json){
+    const destinations = json.destinations;
+    const markupFooter = `<div class='cta read-more'>Read More</div></div></div>`;
+    let delayValue = 0;
+
+    Object.keys(destinations).map(function(value,index){
+        const destination = destinations[value];
+        const cardClass = value + set;
+        const markupHeader = `<div class='card-container ${cardClass}'>`;
+        const card = `<div class='card rounded' style="background-image:url('${destination.img}')">`
+        const heart = `<div class='heart ${destination.favorite}'></div>`
+        const price = `<div class='price'>${destination.price}</div>`;
+        const title =  `<h3>${destination.title}</h3>`;
+        const desc = `<p>${destination.desc}</p>`
+
+        const destinationCard = markupHeader + card+ heart + price + title + desc + markupFooter;
+        els.exploreList.insertAdjacentHTML('beforeend', destinationCard);
+        TweenLite.set(`.${cardClass}`,{opacity:"0"});
+        TweenLite.to(`.${cardClass}`,1,{opacity:1, delay:delayValue});
+        delayValue += .1
+    },)
+    set ++;
 }
 
 function goUpClicked(){
@@ -28,35 +83,36 @@ function goUpClicked(){
 }
 
 function emailSubmitClicked(){
-    if(validateEmail(emailInput.value)){
-        alert("DOING EMAIL REGISTER FOR:: " + emailInput.value);
+    if(validateEmail(els.emailInput.value)){
+        TweenLite.to('.input-container',.5,{autoAlpha:0});
+        TweenLite.set('.input-container',{display:"none", delay:.5});
+        TweenLite.to('.email-success',.5,{autoAlpha:1, delay:.5})
+    }else{
+        alert("Please enter a valid email address");
     }
-    TweenLite.to('.input-container',.5,{autoAlpha:0});
-    TweenLite.set('.input-container',{display:"none", delay:.5});
-    TweenLite.to('.email-success',.5,{autoAlpha:1, delay:.5})
+    
 }
 function emailInputClicked(){
     emailActive = true;
-    emailLabel.className = 'active';
+    els.emailLabel.className = 'active';
 }
 
 function emailBlurHandler(){
-    const emailValue = emailInput.value;
+    const emailValue = els.emailInput.value;
     if(!emailValue) {
-        emailLabel.className = validity.className = validity.innerHTML = '';
+        els.emailLabel.className = els.validity.className = els.validity.innerHTML = '';
     }
 }
 
 function emailKeyPressed(){
-    const emailValue = emailInput.value;
+    const emailValue = els.emailInput.value;
     if(validateEmail(emailValue)){
-        validity.innerHTML = ' - valid';
-        validity.className = 'green';
+        els.validity.innerHTML = ' - valid';
+        els.validity.className = 'green';
     }else{
-        validity.innerHTML = ' - not valid';
-        validity.className = 'red';
+        els.validity.innerHTML = ' - not valid';
+        els.validity.className = 'red';
     }
-    console.log(validateEmail(emailValue))
 }
 
 function validateEmail(address) 
@@ -68,4 +124,3 @@ function validateEmail(address)
     return (false)
 }
 
-console.log(window.apiData);
